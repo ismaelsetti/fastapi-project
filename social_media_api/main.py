79f450faf57a3d64336babe474e16fb316f11a0c
Ismael <ismaelsetti@gmail.com>
2024-01-26 20:49:50 +0100

    Add sentry

--- social_media_api/main.py
+++ social_media_api/main.py
@@ -1,15 +1,30 @@
 import logging
 from contextlib import asynccontextmanager
 
+import sentry_sdk
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI, HTTPException
 from fastapi.exception_handlers import http_exception_handler
 
+from social_media_api.config import config
 from social_media_api.database import database
 from social_media_api.logging_conf import configure_logging
 from social_media_api.routers.post import router as post_router
 from social_media_api.routers.upload import router as upload_router
 from social_media_api.routers.user import router as user_router
+
+sentry_sdk.init(
+    dsn=config.SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
@@ -30,5 +45,5 @@ app.include_router(upload_router)
 
 @app.exception_handler(HTTPException)
 async def http_exception_handle_logging(request, exc):
-    logging.error(f"HTTPException: {exc.status_code} {exc.detail}")
+    logger.error(f"HTTPException: {exc.status_code} {exc.detail}")
     return await http_exception_handler(request, exc)
